e85087fc70c18c4e592ab32a1faa36b7c66c8b9b
이용찬 <lee9890@naver.com>
2020-08-11 19:22:49 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/listview_20200810/datas/Student.kt
@@ -0,0 +1,12 @@
+package kr.co.tjoeun.listview_20200810.datas
+
+class Student(
+    val name : String,
+    val address : String,
+    val isMale : Boolean,
+    val birthYear : Int
+) {
+
+    // 학생이 갖는 하위 정보 목록을
+    // 멤버변수로 만들자
+}
