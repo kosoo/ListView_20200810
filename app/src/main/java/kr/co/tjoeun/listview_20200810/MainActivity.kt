598133b43e88bbc0db6127de6f838f2400dd5f4d
이용찬 <lee9890@naver.com>
2020-08-11 21:39:00 +0900

    리스트뷰 이벤트처리 + 내용물 변경시 새로고침

--- app/src/main/java/kr/co/tjoeun/listview_20200810/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/listview_20200810/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.listview_20200810
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kr.co.tjoeun.listview_20200810.adapters.StudentAdapter
 import kr.co.tjoeun.listview_20200810.datas.Student
@@ -22,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 //        학생 명부에 실제 학생 데이터 추가
-        mStudentList.add(Student("조경진","서울시 은평구",false, 1988))
+        mStudentList.add(Student("조경진","서울시 은평구",true, 1988))
         mStudentList.add(Student("강윤정","경기도 안양시",true, 1991))
         mStudentList.add(Student("구주영","서울시 용산구",true, 1968))
         mStudentList.add(Student("윤은영","경기도 수원시",false, 1992))
@@ -36,6 +37,32 @@ class MainActivity : AppCompatActivity() {
 
 //        xml에 있는 리스트뷰의 어댑터로 지정해주자.
         studentListView.adapter = mStudentAdapter
+
+//        각각의 학생을 누르면, 그 학생의 이름을 토스트로 띄우자
+//        학생을 누른다 => 리스튜뷰의 줄(Item)을 누른다.
+        studentListView.setOnItemClickListener { adapterView, view, position, l ->
+//            i or position 변수 => 어느줄이 눌렸는지 알려준다.
+
+//            position에 맞는 학생 데이터를 가져와서 토스트에 이름을 찍자
+
+            val clickedStudent = mStudentList[position]
+
+            Toast.makeText(this, clickedStudent.name, Toast.LENGTH_SHORT).show()
+        }
+
+//        각줄이 오래 눌렀을때의 별도 이벤트 처리
+        studentListView.setOnItemLongClickListener { adapterView, view, position, l ->
+
+//            position번째 학생을 목록에서 삭제
+            mStudentList.removeAt(position)
+
+//            어댑터에게 새로고침 실행
+            mStudentAdapter.notifyDataSetChanged()
+
+//            오래 눌린 이벤트는 return ture / false 처리를 해줘야 함
+
+            return@setOnItemLongClickListener true
+        }
 
     }
 }
