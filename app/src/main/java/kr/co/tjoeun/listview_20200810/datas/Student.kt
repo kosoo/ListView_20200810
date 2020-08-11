b8f32881095a9639079ad24ee242dbc85cec0df0
이용찬 <lee9890@naver.com>
2020-08-11 20:49:45 +0900

    성별을 String으로 구해주는 기능 추가 + 어댑터에서 활요

--- app/src/main/java/kr/co/tjoeun/listview_20200810/datas/Student.kt
+++ app/src/main/java/kr/co/tjoeun/listview_20200810/datas/Student.kt
@@ -9,4 +9,14 @@ class Student(
 
     // 학생이 갖는 하위 정보 목록을
     // 멤버변수로 만들자
+
+//    성별을 String 형태로 가공해서 출력해주는 기능 - function
+    fun getGenderString() : String {
+        when (this.isMale){
+            true -> {return "남성"}
+            else -> {return "여성"}
+        }
+    }
+}
+
 }
